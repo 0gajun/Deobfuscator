18d1e2150e09825015e5716ef03ade92eed025ae
oga <oga.ivc.s27@gmail.com>
2015-12-31 00:20:58 +0900

    hardcode input file's paths for convenience in developping with visual studio.

--- Deobfuscator/Deobfuscator.cpp
+++ Deobfuscator/Deobfuscator.cpp
@@ -1,30 +1,33 @@
-﻿// Deobfuscator.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
-//
+﻿// Deobfuscator
 
 #include "stdafx.h"
 #include "pe.h"
 #include "trace.h"
 #include <iostream>
-using namespace std;
 
 int main(int argc, char** argv)
 {
-	if (argc != 2) {
-		cout << "invalid arguments" << endl;
-		return -1;
-	}
+	// TODO: use cmd_line option, and parse it
+
+	std::string original_pe_path
+		= "C:\\Users\\ogamal\\Documents\\binary_code_obfuscation_sample\\bin\\overlap_func.exe";
+	std::string trace_file_path
+		= "C:\\Users\\ogamal\\Documents\\trace_data\\overlapping_functions.log";
+	std::string out_put_binary_path
+		= "C:\\Users\\ogamal\\Documents\\hoge_overlap.exe";
+
 	// Read
 	PEReader::Builder builder;
-	PEReader *reader = builder.setInputFilePath(argv[1])->build();
+	PEReader *reader = builder.setInputFilePath(original_pe_path)->build();
 	if (reader == nullptr) {
-		cout << builder.getErrMsg() << endl;
+		std::cout << builder.getErrMsg() << std::endl;
 		exit(1);
 	}
 	PEFormat pe_fmt = reader->getPEFormat();
 
 	// Analyze
 	TraceReader trace_reader;
-	trace_reader.openTraceFile("C:\\Users\\ogamal\\Documents\\trace_data\\overlapping_functions.log");
+	trace_reader.openTraceFile(trace_file_path);
 	std::shared_ptr<TraceData> data = trace_reader.read();
 	TraceAnalyzer analyzer(*data);
 	std::unique_ptr<TraceAnalysisResult> analysis_result = analyzer.analyze();
@@ -33,30 +36,14 @@ int main(int argc, char** argv)
 	PEEditor editor(pe_fmt, std::move(analysis_result));
 	PEFormat *modified_pe = editor.result();
 
-	// Create new section
-	/*
-	std::vector<char> new_code({ (char)(unsigned char)0xC3 });
-	PEEditor::SectionHeaderBuilder b = PEEditor::SectionHeaderBuilder();
-	b.setName((BYTE *)string(".shadow").c_str(), 7)
-		->setPointerToRawData(0xC00)
-		->setSizeOfRawData(0x200)
-		->setVirtualAddress(0x4000)
-		->setVirtualSize(0x1)
-		->setCharacteristcs(0x60000020);
-	IMAGE_SECTION_HEADER shadow_section_header = b.build();
-
-	editor.addSection(shadow_section_header, new_code);
-	PEFormat *modified_pe = editor.result();
-	*/
-
 	// Write
 	PEWriter::Builder writer_builder;
 	PEWriter *writer = writer_builder
-		.setOutputFilePath("\\Users\\ogamal\\Documents\\hoge_overlap.exe")
+		.setOutputFilePath(out_put_binary_path)
 		->setPEFormat(modified_pe)
 		->build();
 	if (writer == nullptr) {
-		cout << writer_builder.getErrMsg() << endl;
+		std::cout << writer_builder.getErrMsg() << std::endl;
 		exit(1);
 	}
 	writer->write();
