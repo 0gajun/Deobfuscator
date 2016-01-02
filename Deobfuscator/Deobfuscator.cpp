27f911251af3db4669586ca66284b97f8b8a12a9
oga <oga.ivc.s27@gmail.com>
2016-01-02 13:44:22 +0900

    enable to pass path of target files.

--- Deobfuscator/Deobfuscator.cpp
+++ Deobfuscator/Deobfuscator.cpp
@@ -6,19 +6,9 @@
 #include "disassembler.h"
 #include <iostream>
 
-int main(int argc, char** argv)
+void deobfuscate(std::string original_pe_path,
+	std::string trace_file_path,std::string output_binary_path)
 {
-	// TODO: use cmd_line option, and parse it
-
-	std::string original_pe_path
-		//= "C:\\Users\\ogamal\\Documents\\binary_code_obfuscation_sample\\bin\\overlap_func.exe";
-		= "C:\\Users\\ogamal\\Documents\\binary_code_obfuscation_sample\\single_overlap_func.exe";
-	std::string trace_file_path
-		//= "C:\\Users\\ogamal\\Documents\\trace_data\\overlapping_functions.log";
-		= "C:\\Users\\ogamal\\Documents\\trace_data\\single_overlaping_func.log";
-	std::string out_put_binary_path
-		= "C:\\Users\\ogamal\\Documents\\hoge_single_overlap.exe";
-
 	// Read
 	PEReader::Builder builder;
 	PEReader *reader = builder.setInputFilePath(original_pe_path)->build();
@@ -50,7 +40,7 @@ int main(int argc, char** argv)
 	// Write
 	PEWriter::Builder writer_builder;
 	PEWriter *writer = writer_builder
-		.setOutputFilePath(out_put_binary_path)
+		.setOutputFilePath(output_binary_path)
 		->setPEFormat(modified_pe)
 		->build();
 	if (writer == nullptr) {
@@ -58,5 +48,27 @@ int main(int argc, char** argv)
 		exit(1);
 	}
 	writer->write();
+	return;
+}
+
+int main(int argc, char** argv)
+{
+	// TODO: use cmd_line option, and parse it
+	std::cout << argc << std::endl;
 	return 0;
+
+	if (argc == 4) {
+		deobfuscate(argv[1], argv[2], argv[3]);
+	}
+	else {
+		std::string original_pe_path
+			//= "C:\\Users\\ogamal\\Documents\\binary_code_obfuscation_sample\\bin\\overlap_func.exe";
+			= "C:\\Users\\ogamal\\Documents\\binary_code_obfuscation_sample\\single_overlap_func.exe";
+		std::string trace_file_path
+			//= "C:\\Users\\ogamal\\Documents\\trace_data\\overlapping_functions.log";
+			= "C:\\Users\\ogamal\\Documents\\trace_data\\single_overlaping_func.log";
+		std::string output_binary_path
+			= "C:\\Users\\ogamal\\Documents\\hoge_single_overlap.exe";
+		deobfuscate(original_pe_path, trace_file_path, output_binary_path);
+	}
 }
